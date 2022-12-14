f1e0708c8cc42e4d76cd977c93bb955fb7cd05dd
Jeisson Felipe Manrique Pulido <jeissonmp15@gmail.com>
2022-12-14 16:19:20 -0500

    Ajuste de base de datos postgresql heroku

--- star_wars_graphql/settings.py
+++ star_wars_graphql/settings.py
@@ -90,9 +90,9 @@ DATABASES = {
     }
 }
 
-# DATABASE_URL = os.environ.get('DATABASE_URL')
-# db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-# DATABASES['default'].update(db_from_env)
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
