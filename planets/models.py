320eb8e712808b77dc5f66b19d68d05f79a438e5
Jeisson Felipe Manrique Pulido <jeissonmp15@gmail.com>
2022-12-14 16:45:44 -0500

    Ajuste modelo planetas, longitud campos

--- planets/models.py
+++ planets/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class Planet(models.Model):
     name = models.CharField(max_length=100)
-    diameter = models.CharField(max_length=20)
-    climate = models.CharField(max_length=20)
-    gravity = models.CharField(max_length=20)
-    terrain = models.CharField(max_length=20)
-    population = models.CharField(max_length=20)
+    diameter = models.CharField(max_length=100)
+    climate = models.CharField(max_length=100)
+    gravity = models.CharField(max_length=100)
+    terrain = models.CharField(max_length=100)
+    population = models.CharField(max_length=100)
